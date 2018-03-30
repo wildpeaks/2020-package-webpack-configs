39224db00b7a3552ad56bbd24e6cc5753f42424f
Cecile Muller <git@wildpeaks.fr>
2018-03-30 12:11:26 +0200

    Added test for Local Modules

--- test/getConfig.spec.js
+++ test/getConfig.spec.js
@@ -52,11 +52,13 @@ beforeAll(() => {
 	server = app.listen(8888);
 	jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 });
+
 afterAll(done => {
 	server.close(() => {
 		done();
 	});
 });
+
 beforeEach(done => {
 	rimraf(outputFolder, () => {
 		mkdirSync(outputFolder);
@@ -92,6 +94,45 @@ it('Basic', async() => {
 				return '#hello not found';
 			}
 			if (el.innerText !== 'Hello World'){
+				return 'Bad #hello.innerText';
+			}
+			return 'ok';
+			/* eslint-enable */
+		});
+		expect(found).toBe('ok', 'DOM tests');
+	} finally {
+		await browser.close();
+	}
+});
+
+
+it('Local Modules', async() => {
+	await testFixture(
+		{
+			rootFolder,
+			outputFolder,
+			entry: {
+				myapp: './local-modules/myapp.ts'
+			},
+			minify: false
+		},
+		[
+			'index.html',
+			'myapp.js',
+			'myapp.js.map'
+		]
+	);
+	const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+	try {
+		const page = await browser.newPage();
+		await page.goto('http://localhost:8888/');
+		const found = await page.evaluate(() => {
+			/* eslint-disable */
+			var el = document.getElementById('hello');
+			if (typeof el === 'undefined'){
+				return '#hello not found';
+			}
+			if (el.innerText !== 'Hello 100123'){
 				return 'Bad #hello.innerText';
 			}
 			return 'ok';
