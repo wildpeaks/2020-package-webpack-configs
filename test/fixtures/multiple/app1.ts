17edf4342b19bf4dba77749a21cd26ad5ada47f2
Cecile Muller <git@wildpeaks.fr>
2018-04-01 08:59:16 +0200

    Added test fixture for multiple entries

---
+++ test/fixtures/multiple/app1.ts
@@ -0,0 +1,8 @@
+/* eslint-env browser */
+import {myclass} from './app1.css';
+
+const mydiv = document.createElement('div');
+mydiv.setAttribute('id', 'hello');
+mydiv.className = myclass;
+mydiv.innerText = 'APP 1';
+document.body.appendChild(mydiv);
