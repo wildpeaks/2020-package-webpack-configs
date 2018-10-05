30b0316433ff66326b5565c339f4f669a92e48ba
Cecile Muller <git@wildpeaks.fr>
2018-10-05 14:16:08 +0200

    Added tests for `rawExtensions`

---
+++ test/params-rawExtensions.spec.js
@@ -0,0 +1,41 @@
+/* eslint-env node, jasmine */
+'use strict';
+const {join} = require('path');
+const getConfig = require('..');
+
+
+/**
+ * @param {String} title
+ * @param {String[]} rawExtensions
+ * @param {Boolean} expectThrows
+ */
+function testFixture(title, rawExtensions, expectThrows){
+	it(title, () => {
+		let actualThrows = false;
+		try {
+			getConfig({
+				entry: {
+					dummy: './src/dummy.ts'
+				},
+				rootFolder: __dirname,
+				outputFolder: join(__dirname, 'dummy'),
+				rawExtensions
+			});
+		} catch(e){
+			actualThrows = true;
+		}
+		expect(actualThrows).toBe(expectThrows);
+	});
+}
+
+testFixture('Valid: []', [], false);
+testFixture('Valid: ["dummy"]', ['dummy'], false);
+testFixture('Invalid: {}', {}, true);
+testFixture('Invalid: 123', 123, true);
+testFixture('Invalid: ""', '', true);
+testFixture('Invalid: "hello"', 'hello', true);
+testFixture('Invalid: NaN', NaN, true);
+testFixture('Invalid: null', null, true);
+testFixture('Invalid: false', false, true);
+testFixture('Invalid: true', true, true);
+testFixture('Invalid: Symbol', Symbol('hello'), true);
