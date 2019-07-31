c343b16840617425ce4dc7d6e3b59c8803f358f0
Cecile Muller <git@wildpeaks.fr>
2019-07-31 10:53:55 +0200

    Added some tests for the new options

---
+++ test/params-cssChunkFilename.spec.js
@@ -0,0 +1,41 @@
+/* eslint-env node, jasmine */
+'use strict';
+const {join} = require('path');
+const getConfig = require('..');
+
+
+/**
+ * @param {String} title
+ * @param {String} cssChunkFilename
+ * @param {Boolean} expectThrows
+ */
+function testFixture(title, cssChunkFilename, expectThrows){
+	it(title, () => {
+		let actualThrows = false;
+		try {
+			getConfig({
+				entry: {
+					dummy: './src/dummy.ts'
+				},
+				rootFolder: __dirname,
+				outputFolder: join(__dirname, 'dummy'),
+				cssChunkFilename
+			});
+		} catch(e){
+			actualThrows = true;
+		}
+		expect(actualThrows).toBe(expectThrows);
+	});
+}
+
+testFixture('Valid: undefined', undefined, false); // eslint-disable-line no-undefined
+testFixture('Valid: ""', '', false);
+testFixture('Valid: "hello"', 'hello', false);
+testFixture('Invalid: 123', 123, true);
+testFixture('Invalid: {}', {}, true);
+testFixture('Invalid: NaN', NaN, true);
+testFixture('Invalid: null', null, true);
+testFixture('Invalid: false', false, true);
+testFixture('Invalid: true', true, true);
+testFixture('Invalid: Promise', Promise.resolve(), true);
+testFixture('Invalid: Symbol', Symbol('hello'), true);
