27f222d56293f7c93bb24ea739ee8e20d60d1121
Cecile Muller <git@wildpeaks.fr>
2019-08-20 14:12:05 +0200

    Added more tests for `scss` property

--- test/fixtures/css-image/myapp.ts
+++ test/fixtures/css-image/myapp.ts
@@ -1,6 +1,11 @@
 /* eslint-env browser */
+import * as css from './myapp.css';
+const {myclass} = css as {
+	myclass: string;
+};
 
 const mydiv = document.createElement('div');
 mydiv.setAttribute('id', 'hello');
+mydiv.className = myclass;
 mydiv.innerText = 'Hello World';
 document.body.appendChild(mydiv);
