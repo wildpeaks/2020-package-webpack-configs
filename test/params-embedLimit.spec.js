db1640d8fc3dd9df5cd43b04b2eafefd5e2a41b5
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-12-14 13:35:33 +0100

    test: convert "params" tests to Mocha

---
+++ test/params-embedLimit.spec.js
@@ -0,0 +1,42 @@
+/* eslint-env node, mocha */
+"use strict";
+const {strictEqual} = require("assert");
+const {join} = require("path");
+const getConfig = require("../packages/webpack-config-web");
+
+/**
+ * @param {String} title
+ * @param {Number} embedLimit
+ * @param {Boolean} expectThrows
+ */
+function testFixture(title, embedLimit, expectThrows) {
+	it(title, () => {
+		let actualThrows = false;
+		try {
+			getConfig({
+				entry: {
+					dummy: "./src/dummy.ts"
+				},
+				rootFolder: __dirname,
+				outputFolder: join(__dirname, "dummy"),
+				embedLimit
+			});
+		} catch (e) {
+			actualThrows = true;
+		}
+		strictEqual(actualThrows, expectThrows);
+	});
+}
+
+describe("embedLimit", () => {
+	testFixture("Valid: 123", 123, false);
+	testFixture('Invalid: ""', "", true);
+	testFixture('Invalid: "hello"', "hello", true);
+	testFixture("Invalid: {}", {}, true);
+	testFixture("Invalid: NaN", NaN, true);
+	testFixture("Invalid: null", null, true);
+	testFixture("Invalid: false", false, true);
+	testFixture("Invalid: true", true, true);
+	testFixture("Invalid: Promise", Promise.resolve(), true);
+	testFixture("Invalid: Symbol", Symbol("hello"), true);
+});
